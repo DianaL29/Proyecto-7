dea67461085e8c04d2f1b3fae7c7d0378a9262b6
Diana Laura <justodianalaura@gmail.com>
2025-02-16 19:37:27 -0600

    Add files via upload

---
+++ Proyecto 7/appy.py
@@ -0,0 +1,3 @@
+import streamlit as pt
+import pandas as pd
+import ploty.express as px[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[hlr g[C[C[C[C[C[C[
